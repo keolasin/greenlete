518828f78014875117fabf7ba6f2d9847778803c
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-07-30 12:15:57 -0700

    test updated for api

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,13 +1,13 @@
 const request = require("request");
-const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const server = require("../../src/server/server.js");
+const base = "http://localhost:5000/testApi";
 
 describe("routes : static", () => {
   describe("GET /", () => {
-    it("should return a status code 200 and the slogan in the body of the response", done => {
+    it("should return a status code 200 and a test JSON object in the body of the response", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("Healthy you, healthy planet");
+        expect(body).toContain("bob");
         done();
       });
     });
