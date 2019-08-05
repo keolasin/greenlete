2ea12b20b7286c5c262de4c8531c07f714c843dd
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-08-05 12:16:18 -0700

    add and update tests for user

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,13 +1,12 @@
 const request = require("request");
 const server = require("../../src/server/server.js");
-const base = "http://localhost:5000/testApi";
+const base = "http://localhost:3306/api/";
 
 describe("routes : static", () => {
-  describe("GET /", () => {
+  describe("GET /testApi", () => {
     it("should return a status code 200 and a test JSON object in the body of the response", done => {
-      request.get(base, (err, res, body) => {
+      request.get(`${base}testApi`, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("bob");
         done();
       });
     });
